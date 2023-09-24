4235d5000c1be6da372228bde7f56778fefde1dc
Sean McEvoy <sfmcevoy@gmail.com>
2023-09-24 09:22:52 -0500

    Remove footer and header links for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -102,8 +102,6 @@ export function Header() {
             </Link>
             <div className="hidden md:flex md:gap-x-6">
               <NavLink href="#features">Features</NavLink>
-              <NavLink href="#testimonials">Testimonials</NavLink>
-              <NavLink href="#pricing">Pricing</NavLink>
             </div>
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
